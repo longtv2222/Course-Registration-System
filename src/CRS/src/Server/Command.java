4e61e7b8777ffa75967acffca644d3ec14c4ba29
Viet Long Ta <long.ta1@ucalgary.ca>
2020-04-17 07:18:59 -0600

    Update

--- src/CRS/src/Server/Command.java
+++ src/CRS/src/Server/Command.java
@@ -5,10 +5,6 @@ import java.io.Serializable;
 public class Command implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	// The different types of message sent by the Client
-	// WHOISIN to receive the list of the users connected
-	// MESSAGE an ordinary message
-	// LOGOUT to disconnect from the Server
 	public static final int SEARCH_COURSE = 0;
 	public static final int ADD_COURSE = 1;
 	public static final int REMOVE_COURSE = 2;
@@ -18,13 +14,11 @@ public class Command implements Serializable {
 	private int type;
 	private String message;
 
-	// constructor
 	public Command(int type, String message) {
 		this.type = type;
 		this.message = message;
 	}
 
-	// getters
 	public int getType() {
 		return type;
 	}
