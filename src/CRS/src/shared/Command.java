15d1289881069d6edb97fef2519c7cd8bb31fe4e
Viet Long Ta <long.ta1@ucalgary.ca>
2020-04-20 12:40:57 -0600

    Added javadoc comment

--- src/CRS/src/shared/Command.java
+++ src/CRS/src/shared/Command.java
@@ -15,19 +15,41 @@ public class Command implements Serializable {
 	public static final int VIEW_ALL_STUDENT = 7;
 	public static final int ADD_NEW_STUDENT = 8;
 	public static final int ADD_NEW_LECTURE = 9;
-
+	/**
+	 * Type of command.
+	 */
 	private int type;
+	/**
+	 * message is an message that contains the data sent along with the command.
+	 */
 	private String message;
 
+	/**
+	 * Constructor of class command that initializes variables accordingly with
+	 * given data.
+	 * 
+	 * @param type    is the type of command.
+	 * @param message is the message that sent along with the command.
+	 */
 	public Command(int type, String message) {
 		this.type = type;
 		this.message = message;
 	}
 
+	/**
+	 * Get the type of the command.
+	 * 
+	 * @return type.
+	 */
 	public int getType() {
 		return type;
 	}
 
+	/**
+	 * Get the message of the command.
+	 * 
+	 * @return message.
+	 */
 	public String getMessage() {
 		return message;
 	}
